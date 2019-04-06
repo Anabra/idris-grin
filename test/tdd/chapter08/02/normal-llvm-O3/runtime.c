6c2d9d97f43017d1516ef7178078796a08b45f11
Anabra <peter.d.podlovics@gmail.com>
2019-04-06 23:16:44 +0200

    Generated and measured binaries for TDD chapters 06 and 08

---
+++ test/tdd/chapter08/02/normal-llvm-O3/runtime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+/*
+TODO: Add statistics.
+*/
+
+extern int64_t _heap_ptr_;
+int64_t grinMain();
+
+int main() {
+  int64_t* heap = malloc(100*1024*1024);
+  _heap_ptr_ = (int64_t)heap;
+  grinMain();
+  printf("used memory: %ld bytes\n", (uint64_t)_heap_ptr_ - (uint64_t)heap);
+  free(heap);
+  return 0;
+}
